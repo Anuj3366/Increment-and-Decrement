d5d4aa3ac7bd3e403033188dbfce31dcc4a34ad3
Anuj Garg <anujgarg3366@gmail.com>
2023-09-16 20:37:19 +0530

    done increment and decrement in js

---
+++ script.js
@@ -0,0 +1,9 @@
+var value = 0;
+function minus() {
+    value--;
+    document.getElementById("display").textContent = value;
+}
+function plus(){
+    value++;
+    document.getElementById("display").textContent = value;
+}
